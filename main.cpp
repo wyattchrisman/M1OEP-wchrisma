8c4691210412137ed1e75d878530bdccd0b57313
wyattchrisman <wchrisma@uvm.edu>
2023-02-05 23:48:39 -0500

    FINAL

--- main.cpp
+++ main.cpp
@@ -8,24 +8,51 @@
 
 using namespace std;
 
+int generate_number();
+bool get_initial_input();
+string ask_again();
+
 int main()
 {
-    int randTime;
+
 
     // Read in all movies to vector
     string filename = "../movies.csv";
     vector<Movie> movies;
     getDataFromFile(filename,movies);
 
-    // Get random number
-    time_t present_time;
-    present_time=time(NULL);
-    randTime = (present_time*present_time)%100;
+
 
     cout << "Welcome to Random Movie Generator!" << endl;
-    cout << "Press any key to generate a movie" << endl;
+    cout << endl;
 
+    int num = generate_number();
+    bool in = get_initial_input();
 
+    cout << movies[num] << endl;
 
 
 }
+
+int generate_number()
+{
+    // Get random number
+    time_t present_time;
+    present_time=time(NULL);
+    return (present_time*present_time)%100;
+}
+
+bool get_initial_input()
+{
+    string stringIn;
+    cout << "Press enter to generate a movie.";
+    getline(cin, stringIn);
+
+    while(stringIn.length() != 0){
+        cout << "Please press enter to continue." << endl;
+        getline(cin, stringIn);
+    }
+    return true;
+}
+
+
